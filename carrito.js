c78871d3136e42128d4b800265ad39b07d48b632
Thomas Barreto <thomasbarreto23@gmail.com>
2023-09-26 18:35:43 -0300

    finished...

--- carrito.js
+++ carrito.js
@@ -11,7 +11,7 @@ function dibujarCarrito() {
     for (const prod of carrito) { 
         const currentURL = window.location.href;
         let img;
-        if ((currentURL.includes('index.html')) || currentURL.includes('https://thomasbarreto.github.io/Proyecto-desarrollo-web/')){
+        if ((currentURL.includes('index.html')) || currentURL == ('https://thomasbarreto.github.io/Proyecto-desarrollo-web/')){
             img = `${prod.img.slice(1)}`;
         } else {
             img = `${prod.img}`;
@@ -65,11 +65,12 @@ function agregarAlCarrito(producto) {
     //VERIFICAR ESTA VALIDACION
     const currentURL = window.location.href;
     let img;
-    if (currentURL.endsWith('index.html') && currentURL === 'https://thomasbarreto.github.io/Proyecto-desarrollo-web') {
-        img = `${producto.img.slice(1)}`;
+    if ((currentURL.includes('index.html')) || currentURL == ('https://thomasbarreto.github.io/Proyecto-desarrollo-web/')){
+        img = `${prod.img.slice(1)}`;
     } else {
-        img = `${producto.img}`;
+        img = `${prod.img}`;
     }
+
 
     carrito.push(producto);
     console.table(carrito);
